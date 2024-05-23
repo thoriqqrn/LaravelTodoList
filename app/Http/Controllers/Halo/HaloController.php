20e6a3e5104c53b5dffc09b41c13be8111e25006
thoriqqrn <thoriqym12@gmail.com>
2024-05-23 11:35:22 +0700

    Initial commit

---
+++ app/Http/Controllers/Halo/HaloController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers\Halo;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class HaloController extends Controller
+{
+    public function index()
+    {
+        return view('todo.app');
+    }
+}
---
+++ routes/web.php
@@ -0,0 +1,26 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Halo\HaloController;
+use App\Http\Controllers\Todo\TodoController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('halo');
+});
+Route::get('/halo', [HaloController::class, 'index']);
+
+Route::get('/todo', [TodoController::class, 'index'])->name('todo');
+Route::post('/todo', [TodoController::class, 'store'])->name('todo.post');
+Route::put('/todo/{id}', [TodoController::class, 'update'])->name('todo.update');
+Route::delete('/todo/{id}', [TodoController::class, 'destroy'])->name('todo.delete');
